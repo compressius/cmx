74ca3048b576a69de5200be6f96a1f938184c88d
CMX Releases <releases@compressi.us>
2026-09-18 13:51:49 +0200

    Update public installer and documentation

--- install-nightly.ps1
+++ install-nightly.ps1
@@ -6,7 +6,7 @@ param(
 $ErrorActionPreference = 'Stop'
 if ($env:OS -ne 'Windows_NT') { throw 'This installer requires Windows.' }
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$releaseVersion = 'v0.2.25-nightly.20260918111011.48fda07a9d20'
+$releaseVersion = 'v0.2.26-nightly.20260918114726.fca19394d125'
 if (!$Version) { $Version = $releaseVersion }
 if ($Version -notmatch '^v\d+\.\d+\.\d+(-nightly\.\d{14}\.[0-9a-f]+)?$') {
     throw 'Download a published CMX installer from https://compressi.us/install.ps1.'
