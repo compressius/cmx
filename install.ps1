327b7f90c1e2cf6198a404a9abd8bd193ca28dea
CMX Releases <releases@compressi.us>
2026-09-18 14:56:53 +0200

    Update public installer and documentation

--- install.ps1
+++ install.ps1
@@ -6,7 +6,7 @@ param(
 $ErrorActionPreference = 'Stop'
 if ($env:OS -ne 'Windows_NT') { throw 'This installer requires Windows.' }
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$releaseVersion = 'v0.2.24'
+$releaseVersion = 'v0.2.25'
 if (!$Version) { $Version = $releaseVersion }
 if ($Version -notmatch '^v\d+\.\d+\.\d+(-nightly\.\d{14}\.[0-9a-f]+)?$') {
     throw 'Download a published CMX installer from https://compressi.us/install.ps1.'
