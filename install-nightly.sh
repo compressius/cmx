13758b97d1ae5ec9ded5ce6f6cf92b620dd4e52e
CMX Releases <releases@compressi.us>
2026-09-18 18:42:26 +0200

    Update public installer and documentation

--- install-nightly.sh
+++ install-nightly.sh
@@ -5,7 +5,7 @@ set -e
 # Usage: curl -sSfL https://compressi.us/install-nightly.sh | sh
 
 REPO="${CMX_RELEASE_REPO:-compressius/cmx}"
-VERSION="${CMX_VERSION:-v0.2.32-nightly.20260918162719.0d39c93cb127}"
+VERSION="${CMX_VERSION:-v0.2.33-nightly.20260918163856.10a7abdb60bc}"
 
 if [ -n "${CMX_INSTALL_DIR:-}" ]; then
   INSTALL_DIR="$CMX_INSTALL_DIR"
