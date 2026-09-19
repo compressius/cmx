a080940fd82a59e319cb1662a78aa5d8122f40de
CMX Releases <releases@compressi.us>
2026-09-20 00:50:53 +0200

    Update public installer and documentation

--- install-testy.sh
+++ install-testy.sh
@@ -8,7 +8,7 @@ set -e
 # Or set GITHUB_TOKEN in the environment.
 
 REPO="${CMX_RELEASE_REPO:-compressius/cmx}"
-VERSION="${CMX_VERSION:-v0.2.38-testy.20260919223127.5bd65e6d550b}"
+VERSION="${CMX_VERSION:-v0.2.38-testy.20260919224709.70cd0ac5553c}"
 
 if [ -n "${CMX_INSTALL_DIR:-}" ]; then
   INSTALL_DIR="$CMX_INSTALL_DIR"
