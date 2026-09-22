204727bfb5243b4bdc32b94a26c2611ff72a1e50
CMX Releases <releases@compressi.us>
2026-09-22 20:37:22 +0200

    Update public installer and documentation

--- install-testy.sh
+++ install-testy.sh
@@ -5,7 +5,7 @@ set -e
 # Usage: curl -sSfL https://compressi.us/install-testy.sh | sh
 
 REPO="${CMX_RELEASE_REPO:-compressius/cmx}"
-VERSION="${CMX_VERSION:-v0.2.39-testy.20260922171201.3d109716875a}"
+VERSION="${CMX_VERSION:-v0.2.39-testy.20260922183042.94cc6d7c9d2f}"
 
 if [ -n "${CMX_INSTALL_DIR:-}" ]; then
   INSTALL_DIR="$CMX_INSTALL_DIR"
